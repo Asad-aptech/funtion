a7553dde58d1d408aae569bbc1661c309871b46b
Asad-aptech <asadquraishiaptech@gmail.com>
2025-03-05 03:30:51 -0800

    added file css and js

---
+++ function.js
@@ -0,0 +1,26 @@
+
+let changeHeading = () => document.getElementById("heading").innerText = "This is a New Heading!";
+
+
+let changeBg = () => document.getElementById("paragraph").style.backgroundColor = "burlywood";
+
+
+let hoverEffect = () => document.getElementById("paragraph").style.color = "blue";
+
+
+let removeHover = () => document.getElementById("paragraph").style.color = "red";
+
+
+let changeParaText = () => document.getElementById("paragraph").innerText = "New paragraph content added!";
+
+
+let changeFontStyle = () => document.getElementById("paragraph").style.fontFamily = "monospace";
+
+
+let rotateImage = () => document.getElementById("image").style.transform = "rotate(20deg)";
+
+
+let increaseTextSize = () => document.getElementById("paragraph").style.fontSize = "22px";
+
+
+let decreaseTextSize = () => document.getElementById("paragraph").style.fontSize = "14px";
